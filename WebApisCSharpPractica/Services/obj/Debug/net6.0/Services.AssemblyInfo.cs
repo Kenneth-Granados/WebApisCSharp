cdb032716c63948a77dd09593d517ff301668d4c
Kenneth Granados <kennethgranados2001@gmail.com>
2024-05-03 16:37:08 -0600

    Arquitectura Inicial2.0

--- WebApisCSharpPractica/Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
+++ WebApisCSharpPractica/Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec376a2d09abd34490eccb4558621da7f7570d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa87d42dccb080e781a0c46f2aea246c476618a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
